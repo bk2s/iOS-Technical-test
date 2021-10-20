1a6f97c68adebf74666d3623a35a4e3c407be75d
bk2s <bk2scom@gmail.com>
2021-10-20 10:29:58 +0300

    Initial Commit

---
+++ iOS Technical test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Technical test
+//
+//  Created by  Stepanok Ivan on 20.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
